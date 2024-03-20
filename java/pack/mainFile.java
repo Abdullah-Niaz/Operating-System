e92ad3b923b98b3a987b45c975cd265560873884
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-03-21 04:13:42 +0500

    java threads

--- java/pack/mainFile.java
+++ java/pack/mainFile.java
@@ -1,6 +1,6 @@
 package pack;
 
-public class hell {
+public class mainFile {
 	public static void main(String[] args) {
 		hel h = new hel();
 		System.out.print("hello world main file calling other dependencies");
